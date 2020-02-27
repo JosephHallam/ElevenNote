044daa0cdccdf4db88086080b34f56856771623f
Joseph Hallam <joehallam13@gmail.com>
2020-02-27 11:14:30 -0500

    added NoteController to post and get notes

--- ElevenNote.Services/NoteService.cs
+++ ElevenNote.Services/NoteService.cs
@@ -11,25 +11,32 @@ namespace ElevenNote.Services
     public class NoteService
     {
         private readonly Guid _userId;
+        //creates a guid for the ID of the note
         public NoteService(Guid userId)
         {
             _userId = userId;
         }
+        //brought in model is used to se the title and the contents of the note
         public bool CreateNote(NoteCreate model)
         {
             var entity = new Note()
             {
+                //use the noteservice to create a guid to set our id equal to
                 OwnerId = _userId,
+                //use the brought in model to set the title and content
                 Title = model.Title,
                 Content = model.Content,
+                //time created is whenever this method is run
                 CreatedUtc = DateTimeOffset.Now
             };
+            //created an instance of the DbContext, added our newly created note, entity, and saved changes.
             using (var ctx = new ApplicationDbContext())
             {
                 ctx.Notes.Add(entity);
                 return ctx.SaveChanges() == 1;
             }
         }
+        //creates a list of created notes in the form of a query
         public IEnumerable<NoteListItem> GetNotes()
         {
             using (var ctx = new ApplicationDbContext())
@@ -47,6 +54,7 @@ namespace ElevenNote.Services
                             CreatedUtc = e.CreatedUtc
                         }
                         );
+                //returns the list of created notes of a user
                 return query.ToArray();
             }
         }
